096a45f8844a115d9d4cbeb082c7ea928c8a6c86
Zhan <zhunussov2002@gmail.com>
2023-12-03 16:42:40 +0100

    Trying to enable navbar

--- js/navbar-component.js
+++ js/navbar-component.js
@@ -6,12 +6,13 @@ class NavbarComponent extends HTMLElement {
     this.attachShadow({ mode: 'open' });
 
     // Import and clone the template content
-    const template = document.getElementById('navbar-component');
+    const template = document.querySelector('template#navbar-component-template');
     const templateContent = template.content.cloneNode(true);
     this.shadowRoot.appendChild(templateContent);
 
     // Access the shadow DOM and set up any event listeners or initial behavior
-    this.shadowRoot.getElementById('name').textContent = this.getAttribute('name');
+    // Note: You may want to remove this line unless you have an element with the ID 'name' in your template.
+    // this.shadowRoot.getElementById('name').textContent = this.getAttribute('name');
   }
 }
 
