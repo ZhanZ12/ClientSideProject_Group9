63b0300ba401cad1cf6f10b845ef9f9fc8d5e651
Zhan <zhunussov2002@gmail.com>
2023-12-03 16:34:56 +0100

    Adding navbar-component

---
+++ js/navbar-component.js
@@ -0,0 +1,19 @@
+class NavbarComponent extends HTMLElement {
+  constructor() {
+    super();
+
+    // Create a shadow root
+    this.attachShadow({ mode: 'open' });
+
+    // Import and clone the template content
+    const template = document.getElementById('navbar-component');
+    const templateContent = template.content.cloneNode(true);
+    this.shadowRoot.appendChild(templateContent);
+
+    // Access the shadow DOM and set up any event listeners or initial behavior
+    this.shadowRoot.getElementById('name').textContent = this.getAttribute('name');
+  }
+}
+
+// Define the custom element
+customElements.define('navbar-component', NavbarComponent);
